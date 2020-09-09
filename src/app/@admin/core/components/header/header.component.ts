50f8babe601cc5445eaea4fb19662bccecfe913f
Carlos Cuautle <cuautlecg@hotmail.com>
2020-09-09 09:53:10 -0500

    conversion de la plantilla Simple Side Bar Startbootstrap en la plantilla del apartado del proyecto

---
+++ src/app/@admin/core/components/header/header.component.ts
@@ -0,0 +1,24 @@
+import { Component, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent{
+
+  toggledValue = true;
+
+  @Output() toggleChange = new EventEmitter<boolean>()
+
+  toggled(){
+    if (this.toggledValue === undefined) {
+      this.toggledValue = true;
+    }
+
+    this.toggledValue = !this.toggledValue;
+    
+    this.toggleChange.emit(this.toggledValue);
+  }
+
+}
--- src/app/@admin/pages/admin.module.ts
+++ src/app/@admin/pages/admin.module.ts
@@ -3,10 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { AdminRoutingModule } from './admin-routing.module';
 import { AdminComponent } from './admin.component';
+import { HeaderComponent } from '../core/components/header/header.component';
+import { SidebarComponent } from '../core/components/sidebar/sidebar.component';
+import { TitleComponent } from '../core/components/title/title.component';
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, HeaderComponent, SidebarComponent, TitleComponent],
   imports: [
     CommonModule,
     AdminRoutingModule
